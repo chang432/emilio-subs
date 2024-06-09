8480d43797cffbd8947cbfe84cbce37a983dd6be
chang432 <andre888chang@gmail.com>
2024-06-09 16:05:30 -0400

    add highlight logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,18 +9,89 @@ import { useState } from 'react'
 function App() {
   const [showHome, setShowHome] = useState(true);
   const [showMenu, setShowMenu] = useState(false);
-  const [showAbout, setShowAbout] = useState(false);
   const [showReviews, setShowReviews] = useState(false);
-  const shows = [["home",setShowHome], ["menu",setShowMenu], ["about",setShowAbout], ["reviews",setShowReviews]]
+  const shows = [["home",setShowHome], ["menu",setShowMenu], ["reviews",setShowReviews]]
+
+  const [highlightHome, setHighlightHome] = useState(true);
+  const [highlightMenu, setHighlightMenu] = useState(false);
+  const [highlightContact, setHighlightContact] = useState(false);
+  const [highlightReviews, setHighlightReviews] = useState(false);
+  const [currentHighlightTab, setCurrentHighlightTab] = useState('home');
+  const allTabHighlights = [['home', setHighlightHome], ['menu', setHighlightMenu], ['contact', setHighlightContact], ['reviews', setHighlightReviews]];
+  const highlights = [highlightHome, highlightMenu, highlightContact, highlightReviews]
+
+  function setPage(tabName) {
+    for (var entry of shows) {
+      if (entry[0] === tabName) {
+        entry[1](true)
+      } else {
+        entry[1](false)
+      }
+    }
+  }
+
+  function scrollPage() {
+    setPage("home")
+    window.scrollBy({
+      top: window.innerHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  function tabClicked(tabName) {
+
+    if (tabName === "contact") {
+      setCurrentHighlightTab('home')
+      setHighlightHome(true)
+      setHighlightMenu(false)
+      setHighlightContact(false)
+      setHighlightReviews(false)
+      scrollPage()
+      return
+    }
+
+    setCurrentHighlightTab(tabName)
+
+    for (var tab of allTabHighlights) {
+      if (tab[0] === tabName) {
+        tab[1](true)
+      } else {
+        tab[1](false)
+      }
+    }
+
+    setPage(tabName)
+  }
+
+  const headerTabHovered = (tabName) => {
+    if (tabName != currentHighlightTab) {
+      for (var tab of allTabHighlights) {
+        if (tab[0] == tabName) {
+          tab[1](true)
+        } 
+      }
+    }
+  }
+
+  const headerTabUnhovered = (tabName) => {
+    if (tabName != currentHighlightTab) {
+      for (var tab of allTabHighlights) {
+        if (tab[0] == tabName) {
+          tab[1](false)
+        } 
+      }
+    }
+  }
 
   return (
     <div>
       {showHome && <div className='flex flex-col h-screen'>
-        <Header shows={shows} />
+        <Header highlights={highlights} tabClicked={tabClicked} headerTabHovered={headerTabHovered} headerTabUnhovered={headerTabUnhovered} />
         <TitleCard />
       </div>}
       {showHome && <HomePage setShowHome={setShowHome} setShowMenu={setShowMenu} />}
-      {!showHome && <Header shows={shows} />}
+      {!showHome && <Header highlights={highlights} tabClicked={tabClicked} headerTabHovered={headerTabHovered} headerTabUnhovered={headerTabUnhovered} />}
       {showMenu && <Menu />}
       {showReviews && <ReviewPage />}
       <Bottom />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const LogoStyle = {
   'color': '#D90606',
@@ -6,39 +6,40 @@ const LogoStyle = {
   'font-weight': '400'
 }
 
-const HeaderBtnStyle = {
-  'color': 'white'
+const SevensLogoStyle = {
+  'color': '#D90606',
+  'font-family': "Goldman",
+  'font-weight': '700',
+  'font-style': 'normal',
+  'letter-spacing': '0.3rem'
 }
 
-const Header = ({shows}) => {
+const Header = ({highlights, tabClicked, headerTabHovered, headerTabUnhovered}) => {
 
-  function setPage(tabName) {
-    for (var entry of shows) {
-      if (entry[0] === tabName) {
-        entry[1](true)
-      } else {
-        entry[1](false)
-      }
-    }
+  const TabHomeStyle = {
+    'color': highlights[0] ? '#D90606' : 'white'
   }
 
-  function scrollPage() {
-    setPage("home")
-    window.scrollBy({
-      top: window.innerHeight,
-      left: 0,
-      behavior: 'smooth',
-    });
+  const TabMenuStyle = {
+    'color': highlights[1] ? '#D90606' : 'white'
+  }
+  
+  const TabContactStyle = {
+    'color': highlights[2] ? '#D90606' : 'white'
+  }
+  
+  const TabReviewsStyle = {
+    'color': highlights[3] ? '#D90606' : 'white'
   }
 
   return (
     <div className="w-full h-32 flex flex-none justify-content justify-between items-center bg-black px-10">
-      <h1 className='text-5xl' style={{...LogoStyle}}>SEVENS</h1>
+      <h1 className='text-5xl' style={{...SevensLogoStyle}}>SEVENS</h1>
       <div className='space-x-12'>
-        <button style={HeaderBtnStyle} onClick={() => setPage("home")}>Home</button>
-        <button style={HeaderBtnStyle} onClick={() => setPage("menu")}>Menu</button>
-        <button style={HeaderBtnStyle} onClick={scrollPage}>Contact</button>
-        <button style={HeaderBtnStyle} onClick={() => setPage("reviews")}>Reviews</button>
+        <button style={TabHomeStyle} onClick={() => tabClicked('home')} onMouseEnter={() => headerTabHovered('home')} onMouseLeave={() => headerTabUnhovered('home')}>Home</button>
+        <button style={TabMenuStyle} onClick={() => tabClicked('menu')} onMouseEnter={() => headerTabHovered('menu')} onMouseLeave={() => headerTabUnhovered('menu')}>Menu</button>
+        <button style={TabContactStyle} onClick={() => tabClicked('contact')} onMouseEnter={() => headerTabHovered('contact')} onMouseLeave={() => headerTabUnhovered('contact')}>Contact</button>
+        <button style={TabReviewsStyle} onClick={() => tabClicked('reviews')} onMouseEnter={() => headerTabHovered('reviews')} onMouseLeave={() => headerTabUnhovered('reviews')}>Reviews</button>
       </div>
     </div>
   )
