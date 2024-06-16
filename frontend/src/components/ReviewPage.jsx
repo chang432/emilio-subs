691f3196028396d3db744efebf82df47b7a92bb3
chang432 <andre888chang@gmail.com>
2024-06-16 14:39:53 -0400

    Integrate Emilio's Pics for carousel and reviews

--- frontend/src/components/ReviewPage.jsx
+++ frontend/src/components/ReviewPage.jsx
@@ -8,9 +8,9 @@ import yelp_logo from "../assets/reviews/yelp_logo.png"
 import yelp_star from "../assets/reviews/yelp_star.png"
 import tripadvisor_logo from "../assets/reviews/tripadvisor_logo.png"
 import tripadvisor_star from "../assets/reviews/tripadvisor_star.png"
-import sub_lucky from "../assets/reviews/sub_lucky.png"
-import sub_misc from "../assets/reviews/sub_misc.png"
-import sub_unforgiven from "../assets/reviews/sub_unforgiven.png"
+import p1 from "../assets/p1.png"
+import p2 from "../assets/p2.png"
+import p3 from "../assets/p3.png"
 
 const ReviewPage = () => {
 
@@ -23,9 +23,9 @@ const ReviewPage = () => {
     return (
         <div className={`flex flex-col items-center justify-center space-y-0.5 my-0.5 mx-0.5 ${fadeIn ? 'animate-fade-in' : ''}`}>
             <h1 className="text-2xl md:text-6xl py-10 md:py-20 w-full text-center text-white bg-black">Reviews</h1>
-            <ReviewCard logo={gmaps_logo} stars={gmaps_star} author={reviews.google[0].author} review={reviews.google[0].review} pic={sub_lucky} />
-            <ReviewCard logo={yelp_logo} stars={yelp_star} author={reviews.yelp[0].author} review={reviews.yelp[0].review} pic={sub_unforgiven} />
-            <ReviewCard logo={tripadvisor_logo} stars={tripadvisor_star} author={reviews.tripadvisor[0].author} review={reviews.tripadvisor[0].review} pic={sub_misc} />
+            <ReviewCard logo={gmaps_logo} stars={gmaps_star} author={reviews.google[0].author} review={reviews.google[0].review} pic={p1} />
+            <ReviewCard logo={yelp_logo} stars={yelp_star} author={reviews.yelp[0].author} review={reviews.yelp[0].review} pic={p2} />
+            <ReviewCard logo={tripadvisor_logo} stars={tripadvisor_star} author={reviews.tripadvisor[0].author} review={reviews.tripadvisor[0].review} pic={p3} />
         </div>
     )
 }
